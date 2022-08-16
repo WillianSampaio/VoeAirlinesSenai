c4a82c6134ffa9ba6de95526b759e9672ae8e0f0
WillianSampaio <willian.sampaio@df.estudante.senai.br>
2022-08-16 10:31:18 -0300

    Atualizando

---
+++ VoeAirlinesSenai/Entities/Aeronave.cs
@@ -0,0 +1,29 @@
+//Namespace é um conjunto de classes
+//Objeti: é uma instâmcia de uma classe
+namespace VoeAirlinesSenai.Entities;
+
+//Classes:é um conjunto de objetos
+//Objetos: é uma intância de uma classe
+public class Aeronave{
+    public Aeronave(string? fabricante, string? modelo, string? codigo)
+    {
+        Fabricante = fabricante;
+        Modelo = modelo;
+        Codigo = codigo;
+    }
+
+    /*
+Propriedades Automáticas
+Caracteristicas do objeto 
+Automático: gera o get set
+Métodos set -atribui
+Métodos get -recupera
+POCO-foco é o objeto
+*/
+    public int Id{get;set;}
+    public string? Fabricante { get; set; }
+    public string? Modelo { get; set; }
+    public string? Codigo { get; set; }
+
+
+}
